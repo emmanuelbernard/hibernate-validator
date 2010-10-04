7711a9775bd6708766ab52ba0dd3fcda19cd956e
Emmanuel Bernard <emmanuel@hibernate.org>
2010-10-04 09:07:20 +0200

    HV-381 Add tests for fail fast

---
+++ hibernate-validator/src/test/java/org/hibernate/validator/test/engine/failFast/FailFastTest.java
@@ -0,0 +1,57 @@
+package org.hibernate.validator.test.engine.failFast;
+
+import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.hibernate.validator.HibernateValidatorFactory;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.test.util.TestUtil;
+import org.testng.annotations.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+import static org.hibernate.validator.test.util.TestUtil.assertCorrectPropertyPaths;
+import static org.hibernate.validator.test.util.TestUtil.assertNumberOfViolations;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class FailFastTest {
+	@Test
+	public void testFailFastSetOnValidatorFactory() {
+		final HibernateValidatorConfiguration configuration = TestUtil.getConfiguration();
+		final ValidatorFactory factory = configuration.failFast().buildValidatorFactory();
+
+		final Validator validator = factory.getValidator();
+		A testInstance = new A();
+
+		Set<ConstraintViolation<A>> constraintViolations = validator.validate( testInstance );
+		assertNumberOfViolations( constraintViolations, 1 );
+	}
+
+	@Test
+	public void testFailFastSetOnValidator() {
+		final HibernateValidatorConfiguration configuration = TestUtil.getConfiguration();
+		final ValidatorFactory factory = configuration.buildValidatorFactory();
+
+		final Validator validator =
+				factory.unwrap(HibernateValidatorFactory.class)
+					.usingHibernateContext()
+						.failFast()
+					.getValidator();
+		A testInstance = new A();
+
+		Set<ConstraintViolation<A>> constraintViolations = validator.validate( testInstance );
+		assertNumberOfViolations( constraintViolations, 1 );
+	}
+
+	class A {
+		@NotNull
+		String b;
+
+		@NotNull @Email
+		String c;
+	}
+}
